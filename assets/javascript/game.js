05b156cd6470f5eb53bbf43cac2faa0126335dbf
SueJStevens <StevensStock@gmail.com>
2018-09-02 13:46:48 -0700

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,12 @@ var game = {
     test: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12],
 
     runningCalc : 0,
+    wins : 0,
+    losses : 0,
 
     randomIntFromInterval : function(min,max) {
-        return Math.floor(Math.random()*(max-min+1)+min);
+        game.matchNum = Math.floor(Math.random()*(max-min+1)+min);
+        return game.matchNum;
     },
 
     shuffle : function(array) {
@@ -35,28 +38,9 @@ var game = {
 
 } //end game object
 
-$(document).ready(function() {
+$(function() {
 
-    //get the initial random number Between!!! lower boundery of 19 and upper boundery of 120 
-    //translation:  min:20, max:119
-    matchNum = (game.randomIntFromInterval(20, 119) );
-    //display MatchNum to user
-    $("#matchNum").text(matchNum);
-
-    //get the initial random number for each crystal which must be "between 1 - 12" 
-    //Developer Note:  This is clearly bad instruction as the number 1 is allowed per the video and so is 12.
-    //question: duplicate allowed? --developer says no
-    //https://www.frankmitchell.org/2015/01/fisher-yates/
-    game.shuffle(game.test);
-    for (var i=0; i<game.crystals.length; i++) {
-
-        //return 1st four values in the shuffled array
-        game.crystalVal[i] = ( game.test[i] );
-        //display but just for development purposes
-        $("#"+game.crystals[i]).text(game.crystalVal[i]);
-        $("#crystal-"+game.crystals[i]).attr("value", game.crystalVal[i]);
-
-    }
+    restart();
 
     //onclick event listener
     $(".crystal").on("click",function(){
@@ -68,16 +52,55 @@ $(document).ready(function() {
         //populate running calc field by passing the value of crystal into the addVal function
         $("#runningCalc").text( game.addVal(x) );
 
-        //Test For Win/Loss
-            //if win increment win
-            //if loss increment loss
-        //change game state to over
-            //pull new random numbers
-
-        
-
-
+        //Test For Win/Loss/still in progress
+        switch(true) {
+            case (game.runningCalc === game.matchNum):
+                game.wins += 1;
+                console.log("Game Wins: "+ game.wins);
+                $("#winCounter").text(game.wins);
+                game.state = "over";
+                restart();
+                break;
+            case (game.runningCalc >= game.matchNum):
+                game.losses += 1;
+                console.log("Game Losses: " +game.losses);
+                $("#lossCounter").text(game.losses);
+                game.state = "over";
+                restart();
+                break;
+            default:
+                console.log("Game in Progress")
+           }    
     });
+
+    function restart() {
+        //Reset Running Total
+        game.runningCalc = 0;
+        $("#runningCalc").text( game.runningCalc );
+
+        //get the initial random number Between!!! lower boundery of 19 and upper boundery of 120 
+        //translation:  min:20, max:119
+        matchNum = (game.randomIntFromInterval(20, 119) );
+        //display MatchNum to user
+        $("#matchNum").text(matchNum);
+
+        //get the initial random number for each crystal which must be "between 1 - 12" 
+        //Developer Note:  This is clearly bad instruction as the number 1 is allowed per the video and so is 12.
+        //question: duplicate allowed? --developer says no
+        //https://www.frankmitchell.org/2015/01/fisher-yates/
+        game.shuffle(game.test);
+        for (var i=0; i<game.crystals.length; i++) {
+
+            //return 1st four values in the shuffled array
+            game.crystalVal[i] = ( game.test[i] );
+            //display but just for development purposes
+            $("#"+game.crystals[i]).text(game.crystalVal[i]);
+            $("#crystal-"+game.crystals[i]).attr("value", game.crystalVal[i]);
+        }
+
+    };
+
+
 
     
 
